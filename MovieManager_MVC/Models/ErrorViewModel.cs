d6f9423adba960294e0d63e4df4da5d2a82237ad
Tomislav Kescec <tomislav.kescec@gmail.com>
2023-02-18 11:09:26 +0100

    Add project files.

---
+++ MovieManager_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieManager_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
